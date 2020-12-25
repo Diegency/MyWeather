796207d2132d5b4e1c51f1c505a8204a90fb8302
asue <1044373541@qq.com>
2020-12-25 22:50:04 +0800

    提交信息

---
+++ app/src/main/java/com/example/myweather/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import com.example.myweather.gson.Weather;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        }
+
+}
